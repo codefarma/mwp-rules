e62311527d9fdfb3c24d32a01333af7f48329cf9
Kevin Carwile <kcarwile@gmail.com>
2018-06-14 10:52:24 -0700

    presentation tweak for rule conditions rows

--- templates/rules/conditions/table_row.php
+++ templates/rules/conditions/table_row.php
@@ -36,18 +36,18 @@ $controller = $table->getController();
 				'default_row_actions' => '',
 			)); ?>
 		</div>
+		<?php if ( $condition->not ) : ?>
+			<span style="vertical-align: 2px" title="This condition evaluates TRUE if the condition is NOT MET" class="label label-warning">NOT</span> 
+		<?php endif ?>
 		<strong class="text-info" style="font-size:1.2em"><?php echo $condition->title ?> </strong> 
 		<?php if ( count( $subconditions ) ) : ?>
 			<span class="label label-info"><?php if ( $condition->compareMode() == 'and' ) : ?>AND ALL SUBCONDITIONS<?php else: ?>OR ANY SUBCONDITION<?php endif ?></span>
 		<?php endif ?>
-		<span style="margin-left: 15px" class="label label-<?php echo $condition->enabled ? 'success' : 'danger' ?> rules-pointer" data-rules-enabled-toggle="condition" data-rules-id="<?php echo $condition->id() ?>">
+		<span style="margin-left: 15px; vertical-align: 2px;" class="label label-<?php echo $condition->enabled ? 'success' : 'danger' ?> rules-pointer" data-rules-enabled-toggle="condition" data-rules-id="<?php echo $condition->id() ?>">
 			<?php echo $condition->enabled ? 'ENABLED' : 'DISABLED' ?>
 		</span>
 		<p>
 			<i class="glyphicon glyphicon-triangle-right"></i> Using: <span class="text-success"><?php echo $definition ? $definition->title : 'Unregistered condition' ?></span> 
-			<?php if ( $condition->not ) : ?>
-				<span title="This condition evaluates TRUE if the condition is NOT MET" class="label label-warning">NOT</span>
-			<?php endif ?>
 		</p>
 	</div>
 	<?php if ( $subconditions ) : ?>
